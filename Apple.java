537f8f3b6b19e0b3b07a1cfbe15f6fde071a3501
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-05-12 08:51:26 -0400

    Added Apple

--- Apple.java
+++ Apple.java
@@ -8,12 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+     public Apple()
+    {
+        GreenfootImage myApple = new GreenfootImage("apple.png");
+        setImage(myApple);
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        //Apple falls downwards.
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x,y);
+        
+        //Remove apple and draw game over when apple gets to bottom
+        //MyWorld world = (MyWorld) getWorld();
+        //if(getY() >= world.getHeight())
+        //{
+            //world.gameOver();
+            //world.removeObject(this);
+        //}
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,17 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
